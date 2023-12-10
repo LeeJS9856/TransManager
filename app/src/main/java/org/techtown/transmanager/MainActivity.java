25382e7c6b1b6197ccb2e6883111eb2871ba1ce8
LeeJS9856 <wnstj1057@naver.com>
2023-12-11 00:01:15 +0900

    Feat : add Internet permission

--- app/src/main/java/org/techtown/transmanager/MainActivity.java
+++ app/src/main/java/org/techtown/transmanager/MainActivity.java
@@ -4,15 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    static RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
 
+        //요청 큐가 없으면 요청 큐 생성하기
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        login();
 
     }
+
+    public void login() {
+        //php url 입력
+        String URL = "http://gm8668.dothome.co.kr/login.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //응답이 되었을때 response로 값이 들어옴
+                Toast.makeText(getApplicationContext(), "응답:" + response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //에러나면 error로 나옴
+                Log.e( "error","에러:" + error.getMessage());
+
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                //php로 설정값을 보낼 수 있음
+                return param;
+            }
+        };
+
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
+
+
+    }
+
+
 }
